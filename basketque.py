d7623cbb3b6de90348c79760324d0549d1da124e
Ridwanyasi <ridwan.yasin@elev.ga.ntig.se>
2025-02-03 02:20:54 +0100

    uppdatering plus en till dag

---
+++ basketque.py
@@ -0,0 +1,172 @@
+import os
+import time
+
+
+
+def start_game():
+    print("Dag 1: Vad vill du göra?")
+    print("1. Gå till träningshallen")
+    print("2. Stanna hemma och sova")
+
+    choice = input("Välj ett alternativ (1/2): ")
+    if choice == "1":
+        träningshallen()
+    elif choice == "2":
+        print("Du stannade hemma och sov. Slut på dagen.")
+    else:
+        print("Ogiltigt val. Försök igen.")
+        start_game()
+
+def träningshallen():
+    print("Du är nu i träningshallen. Vad vill du göra?")
+    print("1. Träna på dribbling")
+    print("2. Träna styrketräning")
+    print("3. Träna på 3-poängare")
+
+    choice = input("Välj ett alternativ (1/2/3): ")
+    if choice == "1":
+        dribbling()
+    elif choice == "2":
+        styrketräning()
+    elif choice == "3":
+        trepoängare()
+
+
+    else:
+        print("Ogiltigt val. Försök igen.")
+        träningshallen()
+
+def dribbling():
+    print("Du tränar på dribbling. Vad vill du göra härnäst?")
+    print("1. Konditionsträning")
+    print("2. Gå hem och vila")
+
+    choice = input("Välj ett alternativ (1/2): ")
+    if choice == "1":
+        konditionsträning()
+    elif choice == "2":
+        print("Du gick hem och vilade. Slut på dagen.")
+    else:
+        print("Ogiltigt val. Försök igen.")
+        dribbling()
+
+def styrketräning():
+    print("Du tränar styrka. Vad vill du göra härnäst?")
+    print("1. Gå tillbaka till träningshallen")
+    print("2. Gå hem och vila")
+
+    choice = input("Välj ett alternativ (1/2): ")
+    if choice == "1":
+        träningshallen()
+    elif choice == "2":
+        print("Du gick hem och vilade. Slut på dagen.")
+    else:
+        print("Ogiltigt val. Försök igen.")
+        styrketräning()
+
+def trepoängare():
+    print("Du tränar på 3-poängsskott. Vad vill du göra härnäst?")
+    print("1. Konditionsträning")
+    print("2. Gå hem och vila")
+
+    choice = input("Välj ett alternativ (1/2): ")
+    if choice == "1":
+        konditionsträning()
+    elif choice == "2":
+        print("Du gick hem och vilade. Slut på dagen.")
+    else:
+        print("Ogiltigt val. Försök igen.")
+        trepoängare()
+
+
+
+
+
+def konditionsträning():
+    print("Du tränar kondition. Bra jobbat!")
+    print("Dagen är slut. Slut på spelet.")
+
+start_game()
+
+tre = R""" |\
+  \O
+   |
+  /|
+ |  \
+    """
+print(tre)
+time.sleep(1)
+os.system("cls")
+skott = R"""   
+
+          
+/|          
+\|=--            o
+   ##
+                   \\
+                /   \O
+               O_/   T
+               T    /|
+               |\  | |
+_______________|_|________
+
+
+    """
+print(skott)
+
+time.sleep(1)
+os.system("cls")
+skott2 = R"""   
+
+          
+/|          o
+\|=--            o
+   ##
+                   \\
+                /   \O
+               O_/   T
+               T    /|
+               |\  | |
+_______________|_|________
+
+
+    """
+print(skott2)
+
+time.sleep(1)
+os.system("cls")
+skott3 = R"""   
+
+        o
+/|           o
+\|=--            o
+   ##
+                   \\
+                /   \O
+               O_/   T
+               T    /|
+               |\  | |
+_______________|_|________
+
+
+    """
+print(skott3)
+
+time.sleep(1)
+os.system("cls")
+skott4 = R"""   
+
+          o
+/|   o        o
+\|=--            o
+   ##
+                   \\
+                /   \O
+               O_/   T
+               T    /|
+               |\  | |
+_______________|_|________
+
+
+    """
+print(skott4)
